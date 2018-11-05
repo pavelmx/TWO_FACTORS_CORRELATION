fe3e0a1587cf12c83a6ff00916a943f642be8f68
yurezorsha <yurez_orsha@mail.ru>
2018-11-05 12:56:52 +0300

    add s, t

--- src/xxy/Main.java
+++ src/xxy/Main.java
@@ -4,19 +4,29 @@ import java.io.IOException;
 
 public class Main {
 
-	private static final String file = "sample.csv";
-	
+	private static final String file = "sample3.csv";
+
 	public static void main(String[] args) throws IOException {
-		
-		ParseCSV p = new  ParseCSV(file);
+
+		ParseCSV p = new ParseCSV(file);
 		XXY xxy = new XXY();
 		p.parse(xxy);
-		System.out.println(" Œ––≈Àﬂ÷»ﬂ x1y " + xxy.corrX1Y());
-		System.out.println(" Œ––≈Àﬂ÷»ﬂ x2y " + xxy.corrX2Y());
-		System.out.println(" Œ––≈Àﬂ÷»ﬂ x1x2 " + xxy.corrX1X2());
-		System.out.println("b0 " + xxy.b0());
-		System.out.println("b1 " + xxy.b1());
-		System.out.println("b2 " + xxy.b2());		
+		p.parseStud();
+		xxy.setY_solve();
+		System.out.println("y = " + XXY.math(xxy.b2()) + "*x2 + " + XXY.math(xxy.b1()) + "*x1 + " + XXY.math(xxy.b0()));
+		System.out.println("r = " + XXY.math(xxy.R()));
+		System.out.println("Tr = " + XXY.math(xxy.Tr()));
+		System.out.println("Tc = " + ParseCSV.student.get(xxy.getX1().size() - 3));
+		System.out.println("b0 = " + XXY.math(xxy.b0()));
+		System.out.println("Sb0 = " + xxy.Sb0());
+		System.out.println("tb0 = " + xxy.b0() / xxy.Sb0());
+		System.out.println("b1 = " + XXY.math(xxy.b1()));
+		System.out.println("Sb1 = " + xxy.Sb1());
+		System.out.println("tb1 = " + xxy.b1() / xxy.Sb1());
+		System.out.println("b2 = " + XXY.math(xxy.b2()));
+		System.out.println("Sb2 = " + xxy.Sb2());
+		System.out.println("tb2 = " + xxy.b2() / xxy.Sb2());
+
 	}
 
 }
--- src/xxy/XXY.java
+++ src/xxy/XXY.java
@@ -3,19 +3,23 @@ package xxy;
 import java.util.ArrayList;
 
 public class XXY {
-	
+
 	private ArrayList<Double> x1;
 	private ArrayList<Double> x2;
 	private ArrayList<Double> y;
 	private ArrayList<Double> y_solve;
-	
+
 	public XXY() {
 		this.x1 = new ArrayList<Double>();
 		this.x2 = new ArrayList<Double>();
 		this.y = new ArrayList<Double>();
-		this.y_solve = new ArrayList<Double>();		
+		this.y_solve = new ArrayList<Double>();
 	}
-		
+
+	static public double math(double x) {
+		return Math.ceil(x * 1000) / 1000;
+	}
+
 	public ArrayList<Double> getX1() {
 		return x1;
 	}
@@ -44,8 +48,13 @@ public class XXY {
 		return y_solve;
 	}
 
-	public void setY_solve(ArrayList<Double> y_solve) {
-		this.y_solve = y_solve;
+	public void setY_solve() {
+
+		int cnt = x1.size();
+		for (int i = 0; i < x1.size(); i++) {
+			y_solve.add(b2() * x2.get(i) + b1() * x1.get(i) + b0());
+		}
+
 	}
 
 	public double avX1() {
@@ -54,16 +63,16 @@ public class XXY {
 		for (int i = 0; i < x1.size(); i++) {
 			sum += x1.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
-	
+
 	public double avX1Y() {
 		double sum = 0;
 		int cnt = x1.size();
 		for (int i = 0; i < x1.size(); i++) {
 			sum += x1.get(i) * y.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
 
 	public double avX2() {
@@ -72,16 +81,16 @@ public class XXY {
 		for (int i = 0; i < x2.size(); i++) {
 			sum += x2.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
-	
+
 	public double avX2Y() {
 		double sum = 0;
 		int cnt = x2.size();
 		for (int i = 0; i < x2.size(); i++) {
 			sum += x2.get(i) * y.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
 
 	public double avY() {
@@ -90,80 +99,141 @@ public class XXY {
 		for (int i = 0; i < y.size(); i++) {
 			sum += y.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
-	
+
 	public double avX1X2() {
 		double sum = 0;
 		int cnt = x1.size();
 		for (int i = 0; i < x1.size(); i++) {
 			sum += x1.get(i) * x2.get(i);
 		}
-		return sum / cnt;		
+		return sum / cnt;
 	}
-	
+
 	public double dispX1() {
 		double sum = 0;
 		int cnt = x1.size();
 		for (int i = 0; i < x1.size(); i++) {
 			sum += Math.pow(x1.get(i), 2);
 		}
-		return sum / cnt - Math.pow(avX1(), 2);		
+		return sum / cnt - Math.pow(avX1(), 2);
 	}
-	
+
 	public double dispY() {
 		double sum = 0;
 		int cnt = y.size();
 		for (int i = 0; i < y.size(); i++) {
 			sum += Math.pow(y.get(i), 2);
 		}
-		return sum / cnt - Math.pow(avY(), 2);		
+		return sum / cnt - Math.pow(avY(), 2);
 	}
-	
+
 	public double dispX2() {
 		double sum = 0;
 		int cnt = x2.size();
 		for (int i = 0; i < x2.size(); i++) {
 			sum += Math.pow(x2.get(i), 2);
 		}
-		return sum / cnt - Math.pow(avX2(), 2);		
+		return sum / cnt - Math.pow(avX2(), 2);
 	}
-	
-	public double sX1() {		
-		return Math.sqrt(dispX1());		
+
+	public double sX1() {
+		return Math.sqrt(dispX1());
 	}
-	
-	public double sX2() {		
-		return Math.sqrt(dispX2());		
+
+	public double sX2() {
+		return Math.sqrt(dispX2());
 	}
-	
-	public double sY() {		
-		return Math.sqrt(dispY());		
+
+	public double sY() {
+		return Math.sqrt(dispY());
 	}
-	
-	public double corrX1Y() {		
-		return (avX1Y()-avX1()*avY())/(sX1()*sY());		
+
+	public double corrX1Y() {
+		return (avX1Y() - avX1() * avY()) / (sX1() * sY());
 	}
-	
-	public double corrX2Y() {		
-		return (avX2Y()-avX2()*avY())/(sX2()*sY());		
+
+	public double corrX2Y() {
+		return (avX2Y() - avX2() * avY()) / (sX2() * sY());
 	}
-	
-	public double corrX1X2() {		
-		return (avX1X2()-avX2()*avX1())/(sX2()*sX1());		
+
+	public double corrX1X2() {
+		return (avX1X2() - avX2() * avX1()) / (sX2() * sX1());
 	}
-	
+
 	public double b1() {
-		double b = (corrX1Y()-corrX2Y()*corrX1X2())/(1-Math.pow(corrX1X2(), 2))*(sY()/sX1());
+		double b = (corrX1Y() - corrX2Y() * corrX1X2()) / (1 - Math.pow(corrX1X2(), 2)) * (sY() / sX1());
 		return b;
 	}
-	
+
 	public double b2() {
-		double b = (corrX2Y()-corrX1Y()*corrX1X2())/(1-Math.pow(corrX1X2(), 2))*(sY()/sX2());
+		double b = (corrX2Y() - corrX1Y() * corrX1X2()) / (1 - Math.pow(corrX1X2(), 2)) * (sY() / sX2());
 		return b;
 	}
-	
-	public double b0() {		
-		return avY()-b1()*avX1()-b2()*avX2();
+
+	public double b0() {
+		return avY() - b1() * avX1() - b2() * avX2();
 	}
+
+	public double R() {
+		return Math.sqrt((Math.pow(corrX1Y(), 2) + Math.pow(corrX2Y(), 2) - 2 * corrX1Y() * corrX2Y() * corrX1X2())
+				/ (1 - Math.pow(corrX1X2(), 2)));
+	}
+
+	public double Tr() {
+		return R() * Math.sqrt((x1.size() - 3) / (1 - R() * R()));
+	}
+
+	public double S02() {
+		double sum = 0;
+		int cnt = y.size();
+		for (int i = 0; i < y.size(); i++) {
+			sum += Math.pow(y.get(i) - y_solve.get(i), 2);
+		}
+		return sum / (cnt - 3);
+
+	}
+
+	public double Sx1x2() {
+		double sum = 0;
+		int cnt = x1.size();
+		for (int i = 0; i < x1.size(); i++) {
+			sum += (x1.get(i) - avX1()) * (x2.get(i) - avX2());
+		}
+		return sum;
+	}
+
+	public double S2x1() {
+		double sum = 0;
+		int cnt = x1.size();
+		for (int i = 0; i < x1.size(); i++) {
+			sum += Math.pow(x1.get(i) - avX1(), 2);
+		}
+		return sum;
+	}
+
+	public double S2x2() {
+		double sum = 0;
+		int cnt = x2.size();
+		for (int i = 0; i < x2.size(); i++) {
+			sum += Math.pow(x2.get(i) - avX2(), 2);
+		}
+		return sum;
+	}
+
+	public double Sb0() {
+		return Math.sqrt((1.0 / (x1.size()))
+				+ ((Math.pow(avX1(), 2) * S2x2() + Math.pow(avX2(), 2) * S2x1() - 2 * avX1() * avX2() * Sx1x2())
+						/ (S2x1() * S2x2() - Math.pow(Sx1x2(), 2))) * S02());
+	}
+
+	public double Sb1() {
+		return Math.sqrt(((S2x2()) / (S2x1() * S2x2() - Math.pow(Sx1x2(), 2))) * S02());
+	}
+
+	public double Sb2() {
+		return Math.sqrt(((S2x1()) / (S2x1() * S2x2() - Math.pow(Sx1x2(), 2))) * S02());
+	}
+
 }
